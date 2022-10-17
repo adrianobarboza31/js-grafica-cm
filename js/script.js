8b5f186870ac9b81180eaf6736da2f0d169078de
adrianobarboza31 <barbozaa022@gmail.com>
2022-10-17 18:10:40 +0200

    add griglia

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Consegna
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// <div class="cella">
+// {/* <span>1</span>
+// </div> */}
+const btn = document.getElementById("btn")
+const griglia= document.getElementById("griglia")
+const field= document.getElementById("game")
+function gioca(){
+console.log("inizio gioco....")
+const numCell=100;
+    function cell(num){
+        console.log(num)
+        const cella= document.createElement("div")
+        cella.className="cella"
+        cella.innerHTML=`<span>${num}</span>`
+        return cella;
+    }
+    function drawGrid(){
+        for(let i=1;i<=100;i++){
+            console.log(i)
+           const cella= cell(i);
+            griglia.appendChild(cella);
+        }
+        field.appendChild(griglia);
+    }
+    drawGrid();
+}
+btn.addEventListener("click", gioca)
